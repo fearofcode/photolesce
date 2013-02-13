323183145bf9cf32b8b50616c690cf8f1764c69b
Warren Henning <warren.henning@gmail.com>
2013-02-13 05:40:58 -0800

    Initial commit w/ secret token removed

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Photolesce::Application.config.session_store :cookie_store, key: '_photolesce_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Photolesce::Application.config.session_store :active_record_store
